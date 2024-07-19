58c3b243d5111c30b2ab125e2a1c11f569b0b75d
XokoukioX <3282076201@qq.com>
2024-07-19 14:33:13 +0800

    Added Vercel SI

--- sidebars.js
+++ sidebars.js
@@ -11,6 +11,9 @@
 
 // @ts-check
 
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
